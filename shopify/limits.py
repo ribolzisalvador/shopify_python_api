7fa4759b17729290bc166e2bec9aa358f1ae479f
Jamie Dwyer <jamie.dwyer@shopify.com>
2018-09-21 17:04:01 -0400

    Add test for Limits

--- shopify/limits.py
+++ shopify/limits.py
@@ -1,30 +1,39 @@
-#!/usr/bin/python
-
-# Imports
 import shopify
 
-CREDIT_LIMIT_HEADER_PARAM = 'x-shopify-shop-api-call-limit'
+class Limits(object):
+    CREDIT_LIMIT_HEADER_PARAM = 'x-shopify-shop-api-call-limit'
 
-# Functions
-def api_credit_limit_param():
-    return shopify.ShopifyResource.connection.response.headers[CREDIT_LIMIT_HEADER_PARAM].split('/')
+    @classmethod
+    def credit_left(cls):
+        return cls.credit_limit() - cls.credit_used()
 
-def credit_left():
-    return credit_limit() - credit_used()
+    available_calls = credit_left
 
-available_calls = credit_left
+    @classmethod
+    def is_credit_maxed(cls):
+        return cls.credit_left() <= 0
 
-def credit_maxed():
-    return credit_left() <= 0
+    is_maxed = is_credit_maxed
 
-maxed = credit_maxed
+    @classmethod
+    def credit_limit(cls):
+        return int(cls._api_credit_limit_param()[1])
 
-def credit_limit():
-    return int(api_credit_limit_param()[1])
+    call_limit = credit_limit
 
-call_limit = credit_limit
+    @classmethod
+    def credit_used(cls):
+        return int(cls._api_credit_limit_param()[0])
 
-def credit_used():
-    return int(api_credit_limit_param()[0])
+    call_count = credit_used
 
-call_count = credit_used
+    @classmethod
+    def _api_credit_limit_param(cls):
+        return cls._response().get(cls.CREDIT_LIMIT_HEADER_PARAM).split('/')
+
+    @classmethod
+    def _response(cls):
+        if not shopify.ShopifyResource.connection.response:
+            shopify.Shop.current()
+
+        return shopify.ShopifyResource.connection.response
